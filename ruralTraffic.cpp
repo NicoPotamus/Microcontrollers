570cedc57bb7f7875eed88643de34921e07414e7
Nico DeMilio <njdemilio@gmail.com>
2025-03-05 13:03:56 -0500

    update rural file

--- ruralTraffic.cpp
+++ ruralTraffic.cpp
@@ -13,6 +13,7 @@
 
 #include "stm32f4xx.h"
 
+
 void delayMs(int n);
 int main(void) {
 	
@@ -21,18 +22,19 @@ int main(void) {
 	GPIOC -> MODER &= 0xffff0000;
 	GPIOC -> MODER |= 0x00000555; // set pc 0 - 6 enabled 6 & 7 are input
 	GPIOC -> ODR &= 0;// clear pins
+	GPIOC -> PUPDR = 0x5000;//put pull up resistors on out switches
 	GPIOC -> ODR = 0B001100;// defaulting NSG EWR
 	
 	//Start cycle
 	while(1){
 		delayMs(3000); // hold the initial 3 seconds
-		while((GPIOC -> IDR & 0x80) == 0 ){} //read EW sensor
+		while((GPIOC -> IDR & 0x80) != 0 ){} //read EW sensor
 		GPIOC -> ODR = 0B001010; // NSY EWR
 		delayMs(1000);
 		
 		GPIOC -> ODR = 0B100001;//NSR EWG
 		delayMs(3000);
-		while((GPIOC -> IDR & 0x40) == 0 ){}//read NS sensor
+		while((GPIOC -> IDR & 0x40) != 0 ){}//read NS sensor
 		
 		GPIOC -> ODR = 0B010001;//NSR EWY
 		delayMs(1000);
